d54bd1da632b104f01a2db2fe76a18f3642e2786
thismars <yhklip990@gmail.com>
2024-01-24 18:22:41 +0900

    update: sort rounds

--- src/organisms/round/index.tsx
+++ src/organisms/round/index.tsx
@@ -15,24 +15,27 @@ interface IProps {
 const Round = ({ roundState, changeModalState, txDataState }: IProps) => {
   const isMobile = useMediaQuery({ query: "(min-width:0px) and (max-width:700px)" });
 
+  const sortRoundsDesc = (rounds: IRoundData[]) => {
+    return roundState.sort((a, b) => b.round - a.round);
+  }
+
   if(roundState.length === 0){
     return <></>
   }else{
     if (isMobile) {
       return <RoundMobile
-        roundState={roundState}
+        roundState={sortRoundsDesc(roundState)}
         changeModalState={changeModalState}
         txDataState={txDataState}
       />
     } else {
       return <RoundDesktop
-        roundState={roundState}
+        roundState={sortRoundsDesc(roundState)}
         changeModalState={changeModalState}
         txDataState={txDataState}
       />
     }
   }
-
 }
 
 export default React.memo(Round);
